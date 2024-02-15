16e04219f2ccab81bb4cfc6ff9f8975c1f0766e5
Gurman Kaur <gkaur5_be22@thapar.edu>
2024-02-15 18:40:22 +0530

    check palindrome string using recursion

--- Recursion/check-palindrome.cpp
+++ Recursion/check-palindrome.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
 
-bool checkPalindrome(int n, int i, int j){
+bool checkPalindrome(string s, int i){
+  if(i>s.length()-i-1) return true;
 
+  return s[i]==s[s.length()-i-1] && checkPalindrome(s,i+1);
 }
 
 int main(){
-  
+  string s="cucucu";
+  cout<<checkPalindrome(s, 0);
 }
